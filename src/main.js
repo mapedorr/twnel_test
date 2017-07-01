9ed55e470c9d3aec1f947c98b973fb3a0885e246
Mateo Robayo <mapedorr@gmail.com>
2017-06-30 23:29:54 -0500

    Added animation system.
    Jump animation working.
    Can change animations with LEFT and RIGHT arrow keys and SPACEBAR.
    Animation can be flipped.

--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@
 var testApp = (function() {
   var constant = (function() {
     var self = this;
-    this.FRAME_TICK = 2;
+    this.FRAME_TICK = 3;
+    this.LEFT_ARROW = 37;
+    this.RIGHT_ARROW = 39;
+    this.SPACEBAR = 32;
     
     return function(constantName) {
         return self[constantName] || null;
@@ -17,7 +20,14 @@ var testApp = (function() {
       currentFrame,
       currentTick,
       spritesheetMap,
-      runAnimation;
+      animations = {
+        idle: [],
+        run: [],
+        jump: []
+      },
+      currentAnimation,
+      jumping,
+      flipAnimation;
 
   function setupRenderer() {
     window.requestAnimFrame = (function () {
@@ -27,7 +37,7 @@ var testApp = (function() {
         window.oRequestAnimationFrame ||
         window.msRequestAnimationFrame ||
         function (callback, element) {
-          window.setTimeout(callback, 1000 / 60); // 24 fps
+          window.setTimeout(callback, 1000 / 24);
         };
     })();
     
@@ -53,62 +63,120 @@ var testApp = (function() {
   }
 
   function createAnimations() {
-    // create run animation
-    runAnimation = spritesheetMap.filter(function(frame, index) {
-      return frame.name.indexOf('run') > -1;
-    });
-    runAnimation.sort(function(a, b) {
-      if (a.name < b.name) return -1;
-      if (a.name > b.name) return 1;
-      return 0;
-    });
+    // fill the frames for each animation
+    for (animation in animations) {
+      if (animations.hasOwnProperty(animation)) {
+        animations[animation] = spritesheetMap.filter(function(frame, index) {
+          return frame.name.indexOf(animation) > -1;
+        });
 
-    renderFrame();
-  }
-
-  function renderFrame() {
-    requestAnimFrame(renderFrame);
-
-    currentTick++;
-    if (currentTick > constant('FRAME_TICK')) {
-      currentTick = 0;
-      currentFrame++;
-      
-      if (currentFrame > runAnimation.length - 1) {
-        currentFrame = 0;
+        animations[animation].sort(function(a, b) {
+          if (a.name < b.name) return -1;
+          if (a.name > b.name) return 1;
+          return 0;
+        });
       }
     }
 
+    currentAnimation = animations.idle;
+    renderFrame();
+  }
+
+  function assignKeyListeners() {
+    window.addEventListener('keydown', function(event) {
+      if (!jumping &&
+          event.keyCode === constant('RIGHT_ARROW') ||
+          event.keyCode === constant('LEFT_ARROW')) {
+        currentAnimation = animations.run;
+
+        if (event.keyCode === constant('LEFT_ARROW')) {
+          flipAnimation = true;
+        }
+        else {
+          flipAnimation = false;
+        }
+      }
+    });
+
+    window.addEventListener('keyup', function(event) {
+      currentFrame = 0;
+
+      if (event.keyCode === constant('SPACEBAR')) {
+        jumping = true;
+        currentAnimation = animations.jump;
+        currentFrame = 0;
+      }
+
+      if (!jumping) {
+        currentAnimation = animations.idle;
+      }
+    });
+  }
+
+  function renderFrame() {
+    if (currentAnimation) {
+      currentTick++;
+      if (currentTick > constant('FRAME_TICK')) {
+        currentTick = 0;
+        currentFrame++;
+        
+        if (currentFrame > currentAnimation.length - 1) {
+          if (jumping) {
+            jumping = false;
+            currentAnimation = animations.idle;
+          }
+          
+          currentFrame = 0;
+        }
+      }
+    }
+
+    requestAnimFrame(renderFrame);
     renderScene();
   }
 
   function renderScene() {
-    // var frameData = runAnimation[currentFrame].split(',');
-
     context.clearRect(0, 0, canvasWidth, canvasHeight);
-    context.drawImage(
-      imgElm,
-      runAnimation[currentFrame].x,
-      runAnimation[currentFrame].y,
-      runAnimation[currentFrame].width,
-      runAnimation[currentFrame].height,
-      0,
-      0,
-      runAnimation[currentFrame].width,
-      runAnimation[currentFrame].height
-    );
+
+    context.save();
+
+    if (flipAnimation) {
+      context.translate(
+        canvasWidth + currentAnimation[currentFrame].width,
+        0
+      );
+      context.scale(-1, 1);
+    }
+
+    if (currentAnimation) {
+      context.drawImage(
+        imgElm,
+        currentAnimation[currentFrame].x,
+        currentAnimation[currentFrame].y,
+        currentAnimation[currentFrame].width,
+        currentAnimation[currentFrame].height,
+        canvasWidth / 2,
+        canvasHeight / 2,
+        currentAnimation[currentFrame].width,
+        currentAnimation[currentFrame].height
+      );
+    }
+
+    context.restore();
   }
 
   function initialize() {
     currentFrame = currentTick = 0;
     canvasWidth = window.innerWidth;
     canvasHeight = window.innerHeight;
+    flipAnimation = false;
 
     imgElm = document.getElementById("tim_sprite");
 
     if (setupRenderer()) {
       readMap(createAnimations);
-      // renderFrame();
+      assignKeyListeners();
+      renderFrame();
     }
   }
 
